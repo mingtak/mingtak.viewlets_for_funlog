e4f6fe2d28e356bb964c0256d82d639b887c5b53
andy fang <andy@mingtak.com.tw>
2015-02-11 14:31:35 +0000

    fork version

--- mingtak/viewlets/viewlets/viewlets.py
+++ mingtak/viewlets/viewlets/viewlets.py
@@ -1,101 +1,23 @@
 from zope.interface import Interface
 from five import grok
-from plone.app.layout.viewlets.interfaces import *
-""" below viewlets manager can use.
-IHTTPHeaders , IHtmlHead, IHtmlHeadLinks, IPortalTop,
-IMainNavigation, IGlobalStatusMessage, IPortalHeader,
-IToolbar, IAboveContent, IAboveContentTitle,
-IDocumentActions,IBelowContentTitle, IAboveContentBody,
-IBelowContentBody, IBelowContent, IPortalFooter, IScripts
-"""
-from zope.viewlet.interfaces import IViewletManager
+from plone.app.layout.viewlets.interfaces import IPortalHeader, IAboveContent, IBelowContentBody
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.app.contenttypes.interfaces import IImage
+
+# viewlet nameing format: ContentType_Position_ViewletFunction
+class IImage_IAboveContent_ResponsiveAd(grok.Viewlet):
+    grok.viewletmanager(IAboveContent)
+    grok.context(IImage)
+    template = ViewPageTemplateFile('templates/responsivead.pt')
+
+    def render(self):
+        return self.template()
 
 
-# Add viewlet manager, IGoogleAD, grok way
-class IGoogleAdResponsive(grok.ViewletManager):
-    grok.name('IGoogleAdResponsive')
+class All_IBelowContentBody_TopBanner(grok.Viewlet):
+    grok.viewletmanager(IBelowContentBody)
+    grok.context(Interface)
+    template = ViewPageTemplateFile('templates/topbanner.pt')
 
-# Add viewlet manager, IAddThis, grok way
-class IAddThis(grok.ViewletManager):
-    grok.name('IAddThis')
-
-# Add viewlet manager, IDisqus, grok way
-class IDisqus(grok.ViewletManager):
-    grok.name('IDisqus')
-
-# Add viewlet manager, IMaybeYouLike, grok way
-class IMaybeYouLike(grok.ViewletManager):
-    grok.name('IMaybeYouLike')
-
-# Add viewlet manager, ICollection, ZCML way
-class ICollection(IViewletManager):
-    """A viewlet manager for collection
-    """
-
-# Add viewlet manager, IHomePage, ZCML way
-class IHomePage(IViewletManager):
-    """A viewlet manager for homepage
-    """
-
-# Add viewlet manager, IWebsiteTop, ZCML way
-class IWebsiteTop(IViewletManager):
-    """A viewlet manager
-    """
-
-
-# Below define customized viewlet
-grok.context(Interface)
-grok.templatedir('templates')
-
-class SocialList(grok.Viewlet):
-    grok.viewletmanager(IWebsiteTop)
-
-class HomePageHero(grok.Viewlet):
-    grok.viewletmanager(IHomePage)
-
-class HomePagePromotionBanner(grok.Viewlet):
-    grok.viewletmanager(IHomePage)
-
-class HomePagePromotionTab(grok.Viewlet):
-    grok.viewletmanager(IHomePage)
-
-"""
-class HomePageOnSale(grok.Viewlet):
-    grok.viewletmanager(IHomePage)
-
-class HomePageVacation(grok.Viewlet):
-    grok.viewletmanager(IHomePage)
-
-class HomePageDress(grok.Viewlet):
-    grok.viewletmanager(IHomePage)
-
-class HomePageBeauty(grok.Viewlet):
-    grok.viewletmanager(IHomePage)
-
-class HomePageHealth(grok.Viewlet):
-    grok.viewletmanager(IHomePage)
-
-class HomePageEntertainment(grok.Viewlet):
-    grok.viewletmanager(IHomePage)
-
-class CollectionContent(grok.Viewlet):
-    grok.viewletmanager(ICollection)
-"""
-
-class AddThisViewlet(grok.Viewlet):
-    grok.viewletmanager(IAddThis)
-
-class Disqus(grok.Viewlet):
-    grok.viewletmanager(IDisqus)
-
-class MaybeYouLike(grok.Viewlet):
-    grok.viewletmanager(IMaybeYouLike)
-
-class GoogleAdResponsive(grok.Viewlet):
-    grok.viewletmanager(IGoogleAdResponsive)
-
-class FooterProductList(grok.Viewlet):
-    grok.viewletmanager(IPortalFooter)
-
-class GoogleAdInFooter(grok.Viewlet):
-    grok.viewletmanager(IPortalFooter)
+    def render(self):
+        return self.template()
